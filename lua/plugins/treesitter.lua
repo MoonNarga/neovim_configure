0ce8e461ede9aff097cc5684af2ef20a4d834142
MoonNarga <moonnarga@outlook.com>
2023-06-28 10:51:15 +0800

    modify treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = "all",
+  ensure_installed = { "c", "lua", "bash", "c_sharp", "cmake", "cpp", "dockerfile", "gitignore", "go", "gomod", "haskell", "html", "java", "javascript", "json", "json5", "kotlin", "llvm", "make", "markdown", "markdown_inline", "nix", "perl", "python", "rust", "toml", "typescript", "vue", "yaml", "tsx", "vim", "vimdoc", "query" },
 
   highlight = { enable = true },
   indent = { enable = true },
